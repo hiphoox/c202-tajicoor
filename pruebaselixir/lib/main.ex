485fb4804720ddd663897cee3c4ee180f4186d5c
Mario <60769915+MarioTabura@users.noreply.github.com>
2020-03-17 12:28:56 -0600

    Add files via upload

---
+++ pruebaselixir/lib/main.ex
@@ -0,0 +1,41 @@
+defmodule Main do
+  @moduledoc """
+  Documentation for `Main`.
+  """
+
+  @doc """
+  Modulo para manejar el resto de los modulos
+
+  ## Examples
+
+      #iex> Main.holaMundo()
+      #"Main // Funcion incompleta"
+
+
+  """
+
+  def holaMundo(nil) do
+    IO.puts "Main // Funcion incompleta"
+  end
+
+  def mainCompleto(nombreArchivo) do
+    IO.puts "- -\tLeyendo archivo...\t - - "
+    archivo = Lector.lectorArchivo(nombreArchivo)
+    IO.puts archivo
+    IO.puts "- -\tEjecutando lexer\t - -"
+    listaTokens = Lexer.lexer(archivo)
+    IO.puts listaTokens
+    IO.puts "- -\t Limpiando lista de tokens\t - - "
+    listaTokens = Lexer.borrarComentarios(listaTokens)
+    IO.puts listaTokens
+    IO.puts "- -\t Parser\t - - "
+    ast=Parser.main(listaTokens)
+    ##IO.puts ast
+    IO.puts "- -\t PrettyPrinting\t - -"
+    Prettyprinter.main(ast)
+    #listaTokens
+  end
+
+
+
+end
---
+++ pruebaselixir/test/pruebaselixir_test.exs
@@ -0,0 +1,8 @@
+defmodule PruebaselixirTest do
+  use ExUnit.Case
+  doctest Pruebaselixir
+
+  test "greets the world" do
+    assert Pruebaselixir.hello() == :world
+  end
+end
