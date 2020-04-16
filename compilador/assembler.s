8822aa902c1aa6471f5a13820ed0914d17e944d3
Mark07 <jimenezcornejomarco@gmail.com>
2020-04-16 02:33:17 -0500

    cambios

--- compilador/assembler.s
+++
@@ -1,4 +0,0 @@
- .globl main
-main:
- movl    $287, %eax
- ret
--- compilador/tajicoor_compiler_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CompilertajicoorTest do
-  use ExUnit.Case
-  doctest Compilertajicoor
-
-  ##test "greets the world" do
-    ##assert SoftfiCompiler.hello() == :world
-  ##end
-end
