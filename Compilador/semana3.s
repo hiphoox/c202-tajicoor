a72cb510ac75a0df3f88bd99dee7b80006522caf
Mark07 <jimenezcornejomarco@gmail.com>
2020-05-30 16:50:54 -0500

    Pruebas .c

---
+++ Compilador/semana3.s
@@ -0,0 +1,12 @@
+	.file	"semana3.c"
+	.text
+	.section	.text.startup,"ax",@progbits
+	.p2align 4,,15
+	.globl	main
+	.type	main, @function
+main:
+	movl	$-7, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ Compilador/semana4.c
@@ -0,0 +1,4 @@
+int main() {
+  //return (2+1)*-3<1;
+  return 1<0||1>0;
+}
