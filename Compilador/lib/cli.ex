a7449c21c96f9b0d6bb005108f683c9c87820bdb
Mark07 <jimenezcornejomarco@gmail.com>
2020-05-30 22:11:39 -0500

    Update cli.ex

--- Compilador/lib/cli.ex
+++ Compilador/lib/cli.ex
@@ -3,19 +3,16 @@ defmodule CompiTajicoor.CLI do
     args
     |> parse_args
     |> response
-    #|> IO.puts()
   end
 
   defp parse_args(args) do
     {opts, word, _} =
       args
-      #|> OptionParser.parse(switches: [upcase: :boolean])
       |> OptionParser.parse(strict: [o: :string, verbose: :boolean, S: :boolean])
     {opts, List.to_string(word)}
   end
 
   defp response({opts, word}) do
-    #if opts[:verbose], do: String.upcase(word), else: word
     CompiTajicoor.mainCLI(word,opts[:verbose],opts[:S],opts[:o])
   end
 end
