a495ee4fa7d1116e1aa0fed7bc675aefd50c5650
Mark07 <jimenezcornejomarco@gmail.com>
2020-05-30 21:56:58 -0500

    ensamblador

--- Compilador/assembler.s
+++ Compilador/assembler.s
@@ -1,6 +1,26 @@
- .globl main
+.globl main
 main:
-  movl $4, %eax
-  neg %eax
-  neg %eax
+  movl $1, %eax
+  push %eax
+  movl $0, %eax
+  pop %ecx
+  cmpl %eax, %ecx
+  movl $0, %eax
+  seta %al
+  cmpl $0, %eax
+  je _orJump2
+  movl $1, %eax
+  jmp _orEnd2
+_orJump2:
+  movl $1, %eax
+  push %eax
+  movl $0, %eax
+  pop %ecx
+  cmpl %eax, %ecx
+  movl $0, %eax
+  setg %al
+  cmpl $0, %eax
+  movl $0, %eax
+  setne %al
+_orEnd2:
   ret
