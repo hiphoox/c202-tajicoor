5e38edf416c317f8edc20d68ec4833839f2ae403
Mark07 <jimenezcornejomarco@gmail.com>
2020-06-16 22:08:25 -0500

    Ultimos ajustes

--- Compilador/assembler.s
+++ Compilador/assembler.s
@@ -1,26 +1,7 @@
  .globl main
 main:
-  movl $1, %eax
-  push %eax
-  movl $0, %eax
-  pop %ecx
-  cmpl %eax, %ecx
-  movl $0, %eax
-  seta %al
-  cmpl $0, %eax
-  je _orJump2
-  movl $1, %eax
-  jmp _orEnd2
-_orJump2:
-  movl $1, %eax
-  push %eax
-  movl $0, %eax
-  pop %ecx
-  cmpl %eax, %ecx
-  movl $0, %eax
-  setg %al
+  movl $5, %eax
   cmpl $0, %eax
   movl $0, %eax
-  setne %al
-_orEnd2:
+  sete %al
   ret
